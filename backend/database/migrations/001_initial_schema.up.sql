d52e848800ccb1164ba983a8d3896e09400867df
DavidDominguez-11 <dominguezdavid396@gmail.com>
2025-03-31 07:35:25 -0600

    Clear files: backend and db

--- backend/database/migrations/001_initial_schema.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS series (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(100) NOT NULL UNIQUE,
-    status ENUM('Plan to Watch', 'Watching', 'Dropped', 'Completed') NOT NULL,
-    last_episode_watched INT DEFAULT 0,
-    total_episodes INT DEFAULT NULL,
-    ranking INT DEFAULT NULL
-);
